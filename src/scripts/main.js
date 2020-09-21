7a1b79ea90a5f70ee82c86f4f7d77fd3f7cfeb2f
DTR <t.terry.rice@gmail.com>
2020-09-21 19:52:47 +0300

    header > roof end

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const dropArrow = document.querySelector('.roof__arrow-box');
+
+dropArrow.addEventListener('click', (event) => {
+  if (event.target.closest('.roof__arrow-box')) {
+    const dropMenu = document.querySelector('.roof__drop-list');
+    const dropRectangle = document.querySelector('.roof__drop-rectangle');
+    // console.log("event", event.target)
+
+    if (dropArrow.classList.contains('roof__arrow-box_active')) {
+      dropArrow.classList.remove('roof__arrow-box_active');
+      dropMenu.classList.remove('roof__drop-list_active');
+      dropRectangle.classList.remove('roof__drop-rectangle_active');
+    }
+    else {
+      dropArrow.classList.add('roof__arrow-box_active');
+      dropMenu.classList.add('roof__drop-list_active');
+      dropRectangle.classList.add('roof__drop-rectangle_active');
+    }
+  }
+});
